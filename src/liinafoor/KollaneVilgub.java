b7df4689ef67d4d2f51eda869eebfd71880386a4
liina <liina.abner@gmail.com>
2015-09-28 16:19:06 +0300

    lisatud KollaneVilgub ja TerveTsykkel

---
+++ src/liinafoor/KollaneVilgub.java
@@ -0,0 +1,18 @@
+package liinafoor;
+
+import foor.Foor;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class KollaneVilgub extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Foor foor = new Foor("üleval", primaryStage);
+        for (int i=0;i<10;i++) {
+            foor.kollane(); //paneb põlema v. kustutab ära
+            foor.paus(2);   //ootab enne järgmisele ringile minekut
+            i++;
+        }
+    }
+}
