22d5cf35d1401abd33538cd06f8ec312b3db50e0
liina <liina.abner@gmail.com>
2015-10-12 11:36:22 +0300

    kustutatud liinafoor package ja Ristmik uuendatud vastavalt KristerV uuendustele ja laevada pommitamise ühe reaga variant

--- src/teema1/Peamurdja2_ristmik.java
+++ src/teema1/Peamurdja2_ristmik.java
@@ -1,8 +1,112 @@
 package teema1;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import lib.Foor;
+
 /**
  * Täpselt nagu oli fooriga, on nüüd ristmikuga. Kuidas esiteks
  * ristmik üles ehitada loe Foori README.md-st: https://github.com/KristerV/javaHarjutused
  */
-public class Peamurdja2_ristmik {
+public class Peamurdja2_ristmik extends Application {
+    public void start(Stage primaryStage) throws Exception {
+        Foor ylal = new Foor(Foor.YLEMINE, primaryStage);
+        Foor all = new Foor(Foor.ALUMINE, primaryStage);
+        Foor parem = new Foor(Foor.VASAK,primaryStage);
+        Foor vasak = new Foor(Foor.PAREM,primaryStage);
+        int j = 3; //tsyklite arv
+        double yp = 0;
+        double pp = 0;
+        kollanevilgub(ylal);
+        kollanevilgub(all);
+        kollanevilgub(parem);
+        kollanevilgub(vasak);
+
+        ylal.vahetaPunast(); //l�litub t��rezhiimi
+        all.vahetaPunast();
+        parem.vahetaPunast();
+        vasak.vahetaPunast();
+        parem.paus(0.5);
+        vasak.paus(0.5);
+        pp += 0.5;
+        while ( j > 0) {
+            ylal.paus(11.5);
+            all.paus(11.5);
+            yp += 11.5;
+            roheliseks(parem); //kestab 5.5
+            roheliseks(vasak);
+
+            parem.paus(0.5); //oota 0.5
+            vasak.paus(0.5);
+            pp += 0.5;
+            punaseks(parem); //kestab 5.5
+            punaseks(vasak);
+
+            parem.paus(11.5);
+            vasak.paus(11.5);
+            pp += 11.5;
+            roheliseks(ylal); //mingi viide tuleb sisse siin - ootaks, et punane hakkaks kohe vilkuma, sest 11 on l�bi
+            roheliseks(all);
+            ylal.paus(0.5);
+            all.paus(0.5);
+            yp += 0.5;
+            punaseks(ylal); //kestab 5.5
+            punaseks(all);
+            j--;
+        }
+        ylal.paus(0.5); //hoiab punast
+        all.paus(0.5);
+        yp += 0.5;
+        ylal.vahetaPunast(); //kustutab punased �ra
+        all.vahetaPunast();
+        parem.vahetaPunast();
+        vasak.vahetaPunast();
+        kollanevilgub(ylal); //ja ��rezhiimile ja l�puks kustub
+        kollanevilgub(all);
+        kollanevilgub(parem);
+        kollanevilgub(vasak);
+        System.out.println("pp" + pp +"yp" + yp);
+    }
+    public void kollanevilgub (Foor x) {
+        for (int i=0;i<8;i++) { //kollane vilkumine
+            x.vahetaKollast();
+            x.paus(0.5);
+            i++;
+        }
+    }
+    public void roheliseks (Foor x)  { //pikkus 5.5
+        double p = 0;
+        for (int i=0;i<8;i++) { //punane hakkab vilkuma
+            x.vahetaPunast();
+            x.paus(0.5);
+            p = p+0.5;
+            i++;
+        }
+        x.vahetaKollast(); //kollaseks
+        x.paus(1);
+        p = p + 1;
+        x.vahetaPunast();
+        x.vahetaKollast();
+        x.vahetaRohelist(); //roheliseks
+        x.paus(0.5);
+        p = p + 0.5;
+        System.out.println("roheliseks paus" + p);
+    }
+    public void punaseks (Foor x)  { //pikkus 5.5
+        double p = 0;
+        for (int i=0;i<9;i++) { //roheline vilkuma
+            x.vahetaRohelist();
+            x.paus(0.5);
+            p = p+0.5;
+            i++;
+        }
+        x.vahetaKollast(); //kollaseks
+        x.paus(1);
+        p = p + 1;
+        x.vahetaKollast();
+        x.vahetaPunast(); //punaseks
+        System.out.println("punaseks paus" + p);
+    }
+
+
 }
--- src/teema1/Peamurdja3_laevad.java
+++ src/teema1/Peamurdja3_laevad.java
@@ -16,4 +16,65 @@ import java.util.Scanner;
  *    nii vertikaalselt kui ka horisontaalselt?
  */
 public class Peamurdja3_laevad {
+    public static void main(String[] args) {
+       /*Scanner kasutaja = new Scanner(System.in);
+        int[] laev = {0,1,0,0,1,0,0,0,1,0};
+        int valmis = 0; //kontrollib kas onläbi või ei
+        while (valmis == 0) {
+            System.out.println("seis" + Arrays.toString(laev));
+            System.out.println("Paku 1-10");
+            int num = kasutaja.nextInt();
+            if (laev[num - 1] == 1) {
+                System.out.println(num + " pihtas");
+                laev[num - 1] = 2;
+            } else {
+                System.out.println(num + " möödas.");
+            }
+
+            //kontroll kas mäng on läbi
+            valmis = 1;
+            for (int j=0;j<10;j++){
+                if (laev[j]==1) {
+                    valmis = 0;
+                    break;
+                }
+            }
+        }
+        System.out.println("seis" + Arrays.toString(laev));
+        System.out.println("Mäng läbi");
+        */
+        Scanner kasutaja = new Scanner(System.in);
+
+        int[] laev = {randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev(), randLaev()};
+        int valmis = 0; //kontrollib kas onläbi või ei
+        while (valmis == 0) {
+            System.out.println("seis" + Arrays.toString(laev));
+            System.out.println("Paku 1-10");
+            int num = kasutaja.nextInt();
+            if (laev[num - 1] == 1) {
+                System.out.println(num + " pihtas");
+                laev[num - 1] = 2;
+            } else if (laev[num - 1] == 2) {
+                System.out.println(num + " juba olid siin");
+            } else {
+                System.out.println(num + " möödas.");
+            }
+
+            //kontroll kas mäng on läbi
+            valmis = 1;
+            for (int j = 0; j < 10; j++) {
+                if (laev[j] == 1) {
+                    valmis = 0;
+                    break;
+                }
+            }
+        }
+        System.out.println("seis" + Arrays.toString(laev));
+        System.out.println("Mäng läbi");
+    }
+    public static int randLaev() {
+        int x = (int) (Math.random()*2);
+        return x;
+    }
+
 }
